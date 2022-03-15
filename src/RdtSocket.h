2dbb9a7b6b17bf972ead325cec36d249f75fa8ca
CJ Williams <cj@cjwilliams.io>
2022-03-15 10:57:30 +0000

    Start RdtSocket.c

--- src/RdtSocket.h
+++ src/RdtSocket.h
@@ -4,4 +4,14 @@
 #ifndef SRC_RDTSOCKET_H_
 #define SRC_RDTSOCKET_H_
 
+#include <inttypes.h>
+#include <netinet/in.h>
+
+typedef struct RdtSocket_s {
+  int sd;
+  struct sockaddr_in addr;
+} RdtSocket_t;
+
+RdtSocket_t* setupRdtSocket_t(const char* hostname, const uint64_t port);
+
 #endif  // SRC_RDTSOCKET_H_
