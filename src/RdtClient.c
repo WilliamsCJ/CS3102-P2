e863bb547ae14b500b3ef28f19df15ce972fa4d0
Christopher Williams <cjdw1@st-andrews.ac.uk>
2022-04-04 11:12:10 +0100

    Report and add timer to RdtClient

--- src/RdtClient.c
+++ src/RdtClient.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "rdt.h"
@@ -10,10 +11,12 @@
 FILE    *file;
 char    *buf;
 uint32_t n;
+struct timespec start;
+struct timespec end;
 
 int main(int argc, char* argv[]) {
   if (argc != 3 && argc != 4) {
-    printf("Usage: ./RdtClient hostname file [debug]\n");
+    printf("Usage: ./RdtClient hostname file [debug | time]\n");
     return -1;
   }
 
@@ -46,8 +49,27 @@ int main(int argc, char* argv[]) {
   fread(buf, sizeof(char), n, file);
   fclose(file);
 
+  if (argc == 4 && strcmp(argv[3], "time") == 0) {
+    if (clock_gettime(CLOCK_REALTIME, &start) < 0) {
+      printf("Error starting timer.\n")
+      return -1.
+    }
+  }
+
   /* Send data over RDT */
   rdtSend(socket, buf, n);
+
+  if (argc == 4 && strcmp(argv[3], "time") == 0) {
+    if (clock_gettime(CLOCK_REALTIME, &end) < 0) {
+      printf("Error starting timer.\n")
+      return -1.
+    }
+
+    double time = (double) end.tv_sec - (double) end.tv_sec;
+    time += ((double) end.tv_nsec - (double) end.tv_nsec) / 10e9;
+
+    printf("Total time %.3fs\n", time);
+  }
 
   /* Clean up and return */
   closeRdtSocket_t(socket);
